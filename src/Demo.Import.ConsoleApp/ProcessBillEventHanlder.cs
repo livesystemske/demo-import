9ee9b15e910d954b193e53f09737860269a1c8dc
Koske Kimutai <koskedk@live.com>
2019-04-01 08:18:10 +0300

    messaging lib

--- src/Demo.Import.ConsoleApp/ProcessBillEventHanlder.cs
+++ src/Demo.Import.ConsoleApp/ProcessBillEventHanlder.cs
@@ -1,22 +1,28 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Demo.Import.Domain.Bills.Events;
+using Demo.Messaging;
 using MediatR;
 
 namespace Demo.Import.ConsoleApp
 {
     public class ProcessBillEventHandler:INotificationHandler<BillInvoiceExtracted>,INotificationHandler<BillDetailsExtracted>
     {
-        public Task Handle(BillInvoiceExtracted notification, CancellationToken cancellationToken)
+        public async Task Handle(BillInvoiceExtracted notification, CancellationToken cancellationToken)
         {
-            Program.BusControl.Publish<BillInvoiceExtracted>(notification);
-            return Task.CompletedTask;
+            var sendToUri = new Uri("rabbitmq://localhost/generate_invoice_queuex");
+            var endPoint = await Program.BusControl.GetSendEndpoint(sendToUri);
+
+            await endPoint.Send<IBillInvoiceExtracted>(notification);
         }
 
-        public Task Handle(BillDetailsExtracted notification, CancellationToken cancellationToken)
+        public async Task Handle(BillDetailsExtracted notification, CancellationToken cancellationToken)
         {
-            Program.BusControl.Publish<BillInvoiceExtracted>(notification);
-            return Task.CompletedTask;
+            var sendToUri = new Uri("rabbitmq://localhost/generate_invoice_queuex");
+            var endPoint = await Program.BusControl.GetSendEndpoint(sendToUri);
+
+            await endPoint.Send<INotification>(notification);
         }
     }
 }
--- src/Demo.Import/Domain/Bills/Events/BillInvoiceExtracted.cs
+++ src/Demo.Import/Domain/Bills/Events/BillInvoiceExtracted.cs
@@ -1,8 +1,9 @@
+using Demo.Messaging;
 using MediatR;
 
 namespace Demo.Import.Domain.Bills.Events
 {
-    public class BillInvoiceExtracted:INotification
+    public class BillInvoiceExtracted:IBillInvoiceExtracted, INotification
     {
         public string InvoiceNo { get;  }
         public string InvoiceLineDetail { get;  }
